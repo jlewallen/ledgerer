59f96882d4e31395cf1340cc83d00c8e255074eb
Jacob Lewallen <jlewallen@gmail.com>
2023-04-18 18:55:40 -0700

    Lint flag when writing JSON.

--- src/json.rs
+++ src/json.rs
@@ -3,14 +3,20 @@ use clap::Args;
 use crate::model::LedgerFile;
 
 #[derive(Debug, Args)]
-pub struct Command {}
+pub struct Command {
+    #[arg(short, long)]
+    lint: bool,
+}
 
-pub fn execute_command(file: &LedgerFile, _cmd: &Command) -> anyhow::Result<()> {
+pub fn execute_command(file: &LedgerFile, cmd: &Command) -> anyhow::Result<()> {
+    if cmd.lint {
+        crate::lint::execute_command(file, &crate::lint::Command {})?;
+    }
+
     let sorted = file
         .iter_transactions_in_order()
         .filter(|t| t.is_simple())
         .collect::<Vec<_>>();
-
     serde_json::to_writer(std::io::stdout(), &sorted)?;
 
     Ok(())
